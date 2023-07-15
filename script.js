8d2ef3b8189d86ff694952586131fcc2ffad0f6a
Shubhanshi-Gaudani <72951422+Shubhanshi-Gaudani@users.noreply.github.com>
2023-07-15 15:43:43 -0700

    added nav bar

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
 // script.js
 
-// Add smooth scrolling effect to anchor links
-document.addEventListener('DOMContentLoaded', function () {
-    const anchorLinks = document.querySelectorAll('a[href^="#"]');
-    for (let anchor of anchorLinks) {
-      anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        const target = document.querySelector(this.getAttribute('href'));
-        if (target) {
-          target.scrollIntoView({
-            behavior: 'smooth',
-          });
-        }
+// Smooth scrolling for the navigation links
+document.querySelectorAll('nav a').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
       });
-    }
   });
-  
+});
